f4bba856ebceff198fb00317b8408df0bbc0382e
Marcus Chan <mcha290@UoA.auckland.ac.nz>
2018-07-20 13:22:30 +1200

    Added close connection logic, fixed logic bugs and added summary

--- packet-loss-Java/PacketLoss.java
+++ packet-loss-Java/PacketLoss.java
@@ -22,45 +22,75 @@ public class PacketLoss {
 		File f = new File("trace-small2.txt");
 		TraceData traceInput = new TraceData(f);
 		HashMap<List<String>, Integer> connectionsDict = new HashMap<>();
-		HashMap<List<String>, HashMap<Integer, Integer>> outOfSeq = new HashMap<>(); //list of packets that are out of sequence, value is an ArrayList of trace line indexes
+		HashMap<List<String>, HashMap<Integer, Integer[]>> outOfSeq = new HashMap<>(); //list of packets that are out of sequence, value is an ArrayList of trace line indexes
 		int counter = 0;
 		for (int i = 0; i < traceInput.traceLines.size(); i++) {
 			String[] line = traceInput.traceLines.get(i);
 			if (line[2].isEmpty() || line[3].isEmpty() || line[4].isEmpty() || line[5].isEmpty()) //if any IP or ports are empty, skip
 				continue;
 			List<String> connectionTuple = Collections.unmodifiableList(Arrays.asList(line[2], line[3], line[4], line[5]));
-			int packetSeqNum = Integer.parseInt(line[13]);
+			int seqNum = Integer.parseInt(line[13]);
 			//counter += 1;
 			//if (counter % 10000 == 0) 
 			//	System.out.println(counter + " lines parsed.");
-			for (List<String> key: outOfSeq.keySet())
-				System.out.println(key); 
+			if (!outOfSeq.isEmpty()) {
+				System.out.println("OutOfSeq packet(s) from:");
+				for (List<String> key: outOfSeq.keySet())
+					System.out.println(key + " " + outOfSeq.get(key));
+			}
 			
-			if (connectionsDict.containsKey(connectionTuple) && packetSeqNum == connectionsDict.get(connectionTuple)) {
-				updateSeqNum(connectionsDict.get(connectionTuple), line, connectionTuple, connectionsDict, outOfSeq);
-			} else if (packetSeqNum == 0) {//Handle new connections
+			if (connectionsDict.containsKey(connectionTuple) && seqNum == connectionsDict.get(connectionTuple)) {
+				updateSeqNum(connectionsDict.get(connectionTuple), seqNum + Integer.parseInt(line[8]) + Integer.parseInt(line[11]), 
+							connectionTuple, connectionsDict, outOfSeq, Integer.parseInt(line[14]));
+				
+			} else if (seqNum == 0) {//Handle new connections
 				connectionsDict.put(connectionTuple, 1);
 			} else { //Handle out-of-sequence packets
 				if (!outOfSeq.containsKey(connectionTuple)) 
 					outOfSeq.put(connectionTuple, new HashMap<>()); //add connection and new HashMap to outOfSeq HashMap
-				outOfSeq.get(connectionTuple).put(packetSeqNum, packetSeqNum + Integer.parseInt(line[8]));// add TCP sequence number and index of trace line to HashMap 
+				outOfSeq.get(connectionTuple).put(seqNum, new Integer[]{seqNum + Integer.parseInt(line[8]) + Integer.parseInt(line[11]), Integer.parseInt(line[14])});
+				// add TCP sequence number as key, add next TCP sequence number & connection close flag as value array
 			}
 			
 		}
+		System.out.println("\nConnections still open:");
+		if (connectionsDict.isEmpty()) {
+			System.out.println("None");
+		} else {
+			for (List<String> key: connectionsDict.keySet())
+				System.out.println(key + " expecting packet " + connectionsDict.get(key));
+		}
+		System.out.println("\nRemaining orphaned out-of-sequence packets:");
+		if (outOfSeq.isEmpty()) {
+			System.out.println("None");
+		} else {
+			for (List<String> key: outOfSeq.keySet()) {
+				for (Integer packetNum: outOfSeq.get(key).keySet()) {
+				System.out.println(key + " with packet no. " + packetNum);
+				}
+			}
+		}
+		
  
 	}
 	
-	public void updateSeqNum(int seqNum, String[] line, List<String> connectionTuple, HashMap<List<String>, Integer> connectionsDict, 
-								HashMap<List<String>, HashMap<Integer, Integer>> outOfSeq) {
-		int nextSeqNum = seqNum + Integer.parseInt(line[8]);
-		System.out.println("For " + connectionTuple + " " + connectionsDict.get(connectionTuple) + " is replaced by " + nextSeqNum);
+	public void updateSeqNum(int seqNum, int nextSeqNum, List<String> connectionTuple, HashMap<List<String>, Integer> connectionsDict, 
+								HashMap<List<String>, HashMap<Integer, Integer[]>> outOfSeq, int closedFlag) {
+		//System.out.println("For " + connectionTuple + " " + connectionsDict.get(connectionTuple) + " is replaced by " + nextSeqNum);
 		connectionsDict.replace(connectionTuple, nextSeqNum); //update value to next expected TCP sequence num
-		if (outOfSeq.containsKey(connectionTuple)) {
-			if (outOfSeq.get(connectionTuple).containsKey(nextSeqNum)) {
-				updateSeqNum(nextSeqNum, line, connectionTuple, connectionsDict, outOfSeq);
-				outOfSeq.remove(nextSeqNum);
-				System.out.println(nextSeqNum + " packet which was out of sequence, removed from outOfSeq HashMap");
-			}
+		
+		//after updating, check if any outOfSeq packets can be reordered
+		if (outOfSeq.containsKey(connectionTuple) && outOfSeq.get(connectionTuple).containsKey(nextSeqNum)) {
+			Integer[] outOfSeqInfo = outOfSeq.get(connectionTuple).remove(nextSeqNum); //remove the reordered packet info from outOfSeq
+			if (outOfSeq.get(connectionTuple).isEmpty())
+				outOfSeq.remove(connectionTuple); //remove the connection from outOfSeq if no more outOfSeq packets
+			//System.out.println(nextSeqNum + " packet from " + connectionTuple + " which was out of sequence, removed from outOfSeq HashMap");
+			updateSeqNum(nextSeqNum, outOfSeqInfo[0], connectionTuple, connectionsDict, outOfSeq, outOfSeqInfo[1]);
+
+		}
+		if (closedFlag == 2) {
+			System.out.println(connectionTuple + " closed");
+			connectionsDict.remove(connectionTuple); //remove connection if closed from in-sequence packet
 		}
 	}
 	
