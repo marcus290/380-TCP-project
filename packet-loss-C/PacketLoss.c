6a83ef20352a3335b80ea702612354b00c59ca35
marcus290 <36244342+marcus290@users.noreply.github.com>
2018-08-13 17:04:58 +1200

    C file starting

---
+++ packet-loss-C/PacketLoss.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(int argc, char *argv[]) {
+    //HashMap<Long, Integer> connectionsDict = new HashMap<>();
+	//HashMap<Long, HashMap<Integer, oOSPacket>> outOfSeq = new HashMap<>(); //hashmap of packets that are out of sequence
+	//ArrayList<Long> closedConnections = new ArrayList<Long>(); //record of connections which have all packets accounted for in sequence and closed
+
+    char defaultFile[] = "trace-small.txt";
+    unsigned int packetCounter = 0;
+	unsigned int byteCounter = 0;
+	unsigned int connectionCounter = 0;
+	unsigned int missingBytes = 0;
+}
